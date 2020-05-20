90043c3b7839d554a803d92a952b86e58c630ea2
tunjiakanbi <tunjiakanbi@gmail.com>
2020-05-20 13:05:59 -0500

    intial react setup

---
+++ moviesearchapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+    <h1 className="title">React Movie Search</h1>
+  </div>
+  );
+}
+
+export default App;
